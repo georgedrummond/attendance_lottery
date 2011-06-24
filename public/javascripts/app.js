9b35e09e2fe088943741e1fe654c6dfd7d9e6690
George Drummond <george@accountsapp.com>
2011-06-24 23:42:26 +0100

    lottery sounds and effects. untested on IE

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,31 +1,70 @@
-$(function(document){       
-  $('#hundreds').click(function(){
-    $(this).html(Lottery.hundreds)
-    $('tr').not('.hundreds_'+Lottery.hundreds).remove()
+$(function(document){
+  soundManager.url = '/swf/';
+  soundManager.flashVersion = 9; // optional: shiny features (default = 8)
+  soundManager.useFlashBlock = false; // optionally, enable when you're ready to dive in
+  // enable HTML5 audio support, if you're feeling adventurous. iPad/iPhone will always get this.
+  // soundManager.useHTML5Audio = true;
+
+  soundManager.onready(function(){
+
+    var bell = soundManager.createSound({
+      id: 'buzzer_sound',
+      url: '/sounds/bell.mp3',
+      autoLoad: true,
+      autoPlay: false
+    });
+
+    var win = soundManager.createSound({
+      id: 'winner_sound',
+      url: '/sounds/win.mp3',
+      autoLoad: true,
+      autoPlay: false
+    });
+     
+    function flash_large_number(){
+      var tens = $('#tens').html(),
+      units = $('#units').html(),
+      hundreds = $('#hundreds').html();
+      if(units == '?' || tens == '?' || hundreds =='?'){
+        bell.play();
+        $('#large_number').html(hundreds+tens+units).fadeIn('fast').delay(2000).fadeOut('fast');
+      } else {
+        var winner = $('tr:first > .student_name').html();
+        var registration_group = $('tr:first > .registration_group').html();
+        $('#winner').html(winner + "<br/>" + registration_group).fadeIn();
+        win.play();
+      }  
+    }
+
+    $('#hundreds').click(function(){
+      $(this).html(Lottery.hundreds);
+      flash_large_number();
+      $('tr').not('.hundreds_'+Lottery.hundreds).remove()
+    });
+  
+    $('#tens').click(function(){
+      $(this).html(Lottery.tens);
+      flash_large_number();
+      $('tr').not('.tens_'+Lottery.tens).remove();
+    });
+
+    $('#units').click(function(){
+      $(this).html(Lottery.units);
+      flash_large_number();
+      $('tr').not('.units_'+Lottery.units).remove();
+    });
+
+    $('#winner').click(function(){
+      $(this).fadeOut();
+    });
   });
-  
-  $('#tens').click(function(){
-    $(this).html(Lottery.tens);
-    $('tr').not('.tens_'+Lottery.tens).remove();
-  });
-  
-  $('#units').click(function(){
-    $(this).html(Lottery.units);
-    $('tr').not('.units_'+Lottery.units).remove();
-  });      
-  
-  $('.student').live('click', function(){
-    $(this).fadeOut('fast', function(){
-      $(this).remove();
-    })
-  });
-  
 });
     
 var Lottery = {
   hundreds:null,
   tens:null,
   units:null,
+  number:null,
   generate_number:function(number){
     var max = number+1;
     //console.log('max = '+max);
@@ -35,6 +74,7 @@ var Lottery = {
     this.hundreds = number_string[0];
     this.tens = number_string[1];
     this.units = number_string[2];
+    this.number = number_string;
     if(random_number == 0){
       this.generate_number(number); // 0 doesnt exist!
     }
